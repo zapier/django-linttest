5fab714c41b1eff3865c492a39d28b6e120cc06f
Harrison Jackson <harrison.jackson@zapier.com>
2017-01-26 16:28:44 -0600

    readme and setuppy confic

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -13,26 +13,12 @@ setup(
     packages=find_packages(),
     install_requires=['gitpython==2.1.1', 'flake8==3.2.1', 'pep8==1.7.0', 'pyflakes==1.3.0'],
     include_package_data=True,
-    license='BSD License',  # example license
+    license='',
     description='A django lint test runner',
     long_description=README,
-    url='https://www.zapier.com/',
-    author='Zapier',
-    author_email='yourname@zapier.com',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+    url='https://github.com/zapier/django-linttest',
+    download_url='https://github.com/zapier/django-linttest/tarball/0.1',
+    author='Harrison Jackson',
+    author_email='harrison.jackson@zapier.com',
+    classifiers=[],
 )
